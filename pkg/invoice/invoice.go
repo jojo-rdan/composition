fdf57f306cdb9128ed10194f98d48690d59cecd1
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-08-01 21:30:05 -0500

    packagesWorkingNow

--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -1,8 +1,15 @@
 package invoice
 
+import (
+	"github.com/jojo-rdan/composition/tree/master/pkg/customer"
+	"github.com/jojo-rdan/composition/tree/master/pkg/invoiceitem"
+)
+
+//Los corchetes implican que la relación es de uno a muchos
 type Invoice struct {
 	country string
 	city    string
 	total   float64
 	client  customer.Customer
+	items   []invoiceitem.Item
 }
